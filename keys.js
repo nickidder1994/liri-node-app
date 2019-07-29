fc4c0daf318db7fd025e1beb1a94e84fdef21ac2
Nick Kidder <nickidder@yahoo.com>
2019-07-28 17:16:47 -0700

    fixed axios

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.c00f453e0aaa4f9eaac1fd73bad9daf0
-  secret: process.env.eefaf2879154c63983b026e4f0d382a
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-
 var axios = require('axios');
+var Spotify = require('node-spotify-api');
 
+var userInput = process.argv.slice(2).join(" ")
+//  var selection = process.argv[2]
+
+var spotify = new Spotify(keys.spotify)
+
+   
+  spotify.search({ type: 'track', query: userInput }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
