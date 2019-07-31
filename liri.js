91dc86d50983ea03ac1d6cac56ce3c605e804dba
Nick Kidder <nickidder@yahoo.com>
2019-07-30 18:06:48 -0700

    added ombd api key

--- liri.js
+++ liri.js
@@ -8,11 +8,20 @@ var userInput = process.argv.slice(2).join(" ")
 
 var spotify = new Spotify(keys.spotify)
 
-   
-  spotify.search({ type: 'track', query: userInput }, function(err, data) {
+
+spotify.search({
+    type: 'track',
+    query: userInput
+}, function (err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+        return console.log('Error occurred: ' + err);
     }
-   
-  console.log(data); 
-  });
+
+    console.log(data);
+});
+
+
+
+
+
+//   var queryURL = "http://www.ombd.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
