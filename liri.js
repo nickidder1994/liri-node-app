f892a0594efb84dd5b177e1d81170809bad85772
Nick Kidder <nickidder@yahoo.com>
2019-08-03 09:37:47 -0700

    added working spotify api

--- liri.js
+++ liri.js
@@ -3,12 +3,12 @@ var keys = require("./keys.js");
 var axios = require('axios');
 var Spotify = require('node-spotify-api');
 
-var userInput = process.argv.slice(2).join(" ")
-//  var selection = process.argv[2]
+var userInput = process.argv.slice(3).join(" ")
+ var selection = process.argv[2]
 
 var spotify = new Spotify(keys.spotify)
-
-
+if(selection === 'spotify-this-song')
+{
 spotify.search({
     type: 'track',
     query: userInput
@@ -17,9 +17,9 @@ spotify.search({
         return console.log('Error occurred: ' + err);
     }
 
-    console.log(data);
+    console.log(data.tracks.items[0].album.artists[0].name);
 });
-
+}
 
 
 
